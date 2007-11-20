80d0ea8996a1c8889c3302c58d2400d6330278f0
Martijn van Oosterhout <kleptog@gmail.com>
2007-11-20 08:51:03 +0000

    Restructure the projection code so it can support more projections, primarily the true spherical mercator used by Google, TileCache and others. Add the -m option to select this. The default is still the incorrect projection used before. Also display the used projection during processing. Finally some minor cleanups to fix some warnings about undefined functions.

--- reprojection.c
+++ reprojection.c
@@ -9,12 +9,37 @@
 #include <unistd.h>
 #include <proj_api.h>
 
-static projPJ pj_ll, pj_merc;
+#include "reprojection.h"
 
-void project_init(void)
+static projPJ pj_ll, pj_merc;
+static enum Projection Proj;
+const struct Projection_Info Projection_Info[] = {
+  [PROJ_LATLONG] = { 
+     descr: "Latlong", 
+     proj4text: "(none)", 
+     srs:4326, 
+     option: "-l" },
+  [PROJ_MERC]    = { 
+     descr: "OSM (false) Mercator", 
+     proj4text: "+proj=merc +datum=WGS84  +k=1.0 +units=m +over +no_defs", 
+     srs:3395, 
+     option: "" },
+  [PROJ_SPHERE_MERC] = { 
+     descr: "Spherical Mercator",  
+     proj4text: "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over", 
+     srs:900913, 
+     option: "-m" }
+};
+
+void project_init(enum Projection proj)
 {
+	Proj = proj;
+	
+	if( proj == PROJ_LATLONG )
+		return;
+		
 	pj_ll   = pj_init_plus("+proj=latlong +ellps=GRS80 +no_defs");
-	pj_merc = pj_init_plus("+proj=merc +datum=WGS84  +k=1.0 +units=m +over +no_defs");
+	pj_merc = pj_init_plus( Projection_Info[proj].proj4text );
 			
 	if (!pj_ll || !pj_merc) {
 		fprintf(stderr, "Projection code failed to initialise\n");
@@ -24,18 +49,26 @@ void project_init(void)
 
 void project_exit(void)
 {
+	if( Proj == PROJ_LATLONG )
+		return;
+		
 	pj_free(pj_ll);
 	pj_ll = NULL;
 	pj_free(pj_merc);
 	pj_merc = NULL;
 }
 
+struct Projection_Info const *project_getprojinfo(void)
+{
+  return &Projection_Info[Proj];
+}
+
 void reproject(double *lat, double *lon)
 {
 	double x[1], y[1], z[1];
-	//projUV p, q;
-	//p.u = *lat * DEG_TO_RAD;
-	//p.v = *lon * DEG_TO_RAD;
+	
+	if( Proj == PROJ_LATLONG )
+		return;
 
 	x[0] = *lon * DEG_TO_RAD;
 	y[0] = *lat * DEG_TO_RAD;
