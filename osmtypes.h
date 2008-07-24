fa0dece52b4f0fa811e1cb62fbf672755eb59f3c
jonb <jonb@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2008-07-24 00:07:30 +0000

    osm2pgsql: Move type definitions to a more appropriate location
    git-svn-id: http://svn.openstreetmap.org/applications/utils/export/osm2pgsql@9265 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- osmtypes.h
+++ osmtypes.h
@@ -3,11 +3,19 @@
 #ifndef OSMTYPES_H
 #define OSMTYPES_H
 
+enum OsmType { OSMTYPE_WAY, OSMTYPE_NODE, OSMTYPE_RELATION };
 
 struct osmNode {
     double lon;
     double lat;
 };
+
+struct member {
+    enum OsmType type;
+    int id;
+    char *role;
+};
+
 
 /* exit_nicely - called to cleanup after fatal error */
 void exit_nicely(void);
