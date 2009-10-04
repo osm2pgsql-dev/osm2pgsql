2b92446f64864a7109ccc23c1dfefd885f561e19
jonb <jonb@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2009-10-04 12:47:56 +0000

    Update projection strings to match proj-4.7.1 definitions.
    git-svn-id: http://svn.openstreetmap.org/applications/utils/export/osm2pgsql@17981 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- reprojection.c
+++ reprojection.c
@@ -27,7 +27,7 @@ const struct Projection_Info Projection_Info[] = {
      option: "-M" },
   [PROJ_SPHERE_MERC] = { 
      descr: "Spherical Mercator",  
-     proj4text: "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over", 
+     proj4text: "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs", 
      srs:900913, 
      option: "-m" }
 };
@@ -43,7 +43,7 @@ void project_init(int proj)
 	if( proj == PROJ_LATLONG )
 		return;
 		
-	pj_ll   = pj_init_plus("+proj=latlong +ellps=GRS80 +no_defs");
+	pj_ll   = pj_init_plus("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs");
 	if( proj >= 0 && proj < PROJ_COUNT )
 		pj_merc = pj_init_plus( Projection_Info[proj].proj4text );
 	else if( proj < 0 )
