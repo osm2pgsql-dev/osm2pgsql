8de7a5831e0d9dcfb0db2e198b765f5549ace910
twain <twain@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-05-27 15:43:49 +0000

    move string replacements to c module
    git-svn-id: http://svn.openstreetmap.org/applications/utils/export/osm2pgsql@21464 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- gazetteer/gazetteer.c
+++ gazetteer/gazetteer.c
@@ -8,6 +8,9 @@ PG_MODULE_MAGIC;
 #endif
 
 Datum transliteration( PG_FUNCTION_ARGS );
+Datum gettokenstring( PG_FUNCTION_ARGS );
+void str_replace(char* buffer, int* len, int* changes, char* from, int fromlen, char* to, int tolen, int);
+void str_dupspaces(char* buffer);
 
 PG_FUNCTION_INFO_V1( transliteration );
 Datum
@@ -91,25 +94,25 @@ transliteration( PG_FUNCTION_ARGS )
                 }
                 else if ((*sourcedata & 0xfc) == 0xf8)
                 {
-			/* table does not extend beyond 4 char long, just skip */
+			// table does not extend beyond 4 char long, just skip
 			if (sourcedatalength < 5) break;
 			sourcedatalength -= 5;
 		}
                 else if ((*sourcedata & 0xfe) == 0xfc)
                 {
-			/* table does not extend beyond 4 char long, just skip */
+			// table does not extend beyond 4 char long, just skip
 			if (sourcedatalength < 6) break;
 			sourcedatalength -= 6;
 		}
                 else
                 {
-			/* assume lenngth 1, silently drop bogus characters */
+			// assume lenngth 1, silently drop bogus characters
                         sourcedatalength--;
                 }
         }
         *wchardata = 0;
 
-	/* calc the length of transliteration string */
+	// calc the length of transliteration string
 	resultdatalength = 0;
 	wchardata = wchardatastart;
 	while(*wchardata)
@@ -118,7 +121,7 @@ transliteration( PG_FUNCTION_ARGS )
 		wchardata++;
 	}
 
-	/* allocate & create the result */
+	// allocate & create the result
 	result = (text *)palloc(resultdatalength + VARHDRSZ);
 	SET_VARSIZE(result, resultdatalength + VARHDRSZ);
 	resultdata = (unsigned char *)VARDATA(result);
@@ -144,9 +147,138 @@ transliteration( PG_FUNCTION_ARGS )
 		}
 		wchardata++;
 	}
-//	*resultdata = 0;
 
 	pfree(wchardatastart);
+
+	PG_RETURN_TEXT_P(result);
+}
+
+void str_replace(char* buffer, int* len, int* changes, char* from, int fromlen, char* to, int tolen, int isspace)
+{
+        char *p;
+
+        // Search string is too long to be pressent
+        if (fromlen > *len) return;
+
+        p = strstr(buffer, from);
+        while(p)
+        {
+                if (!isspace || *(p-1) != ' ')
+                {
+                        (*changes)++;
+                        if (tolen != fromlen) memmove(p+tolen, p+fromlen, *len-(p-buffer)+1);
+                        memcpy(p, to, tolen);
+                        *len += tolen - fromlen;
+                }
+                p = strstr(p+1, from);
+        }
+}
+
+void str_dupspaces(char* buffer)
+{
+        char *out;
+        int wasspace;
+
+        out = buffer;
+        wasspace = 0;
+        while(*buffer)
+        {
+                if (wasspace && *buffer != ' ') wasspace = 0;
+                if (!wasspace)
+                {
+                        *out = *buffer;
+                        out++;
+                        wasspace = (*buffer == ' ');
+                }
+                buffer++;
+        }
+        *out = 0;
+}
+
+PG_FUNCTION_INFO_V1( gettokenstring );
+Datum
+gettokenstring( PG_FUNCTION_ARGS )
+{
+	text *source;
+	unsigned char *sourcedata;
+	int sourcedatalength;
+
+	char * buffer;
+	int len;
+	int changes;
+
+	text *result;
+
+	if (GetDatabaseEncoding() != PG_UTF8) 
+	{
+		ereport(ERROR,
+                                        (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+                                         errmsg("requires UTF8 database encoding")));
+	}
+
+	if (PG_ARGISNULL(0))
+	{
+		PG_RETURN_NULL();
+	}
+
+	// The original string
+	source = PG_GETARG_TEXT_P(0);
+	sourcedata = (unsigned char *)VARDATA(source);
+	sourcedatalength = VARSIZE(source) - VARHDRSZ;
+
+	// Buffer for doing the replace in - string could get slightly longer (double is mastive overkill)
+	buffer = (char *)palloc((sourcedatalength*2)*sizeof(char));
+	memcpy(buffer, sourcedata, sourcedatalength);
+	buffer[sourcedatalength] = 0;
+	len = sourcedatalength+1;
+
+	changes = 1;
+	str_dupspaces(buffer);
+	while(changes)
+	{
+		changes = 0;
+		#include <tokenstringreplacements.inc>
+		str_dupspaces(buffer);
+	}
+
+	// 'and' in various languages
+	str_replace(buffer, &len, &changes, " and ", 5, " ", 1, 0);
+	str_replace(buffer, &len, &changes, " und ", 5, " ", 1, 0);
+	str_replace(buffer, &len, &changes, " en ", 4, " ", 1, 0);
+	str_replace(buffer, &len, &changes, " et ", 4, " ", 1, 0);
+	str_replace(buffer, &len, &changes, " e ", 3, " ", 1, 0);
+	str_replace(buffer, &len, &changes, " y ", 3, " ", 1, 0);
+
+	// 'the' (and similar)
+	str_replace(buffer, &len, &changes, " the ", 5, " ", 1, 0);
+	str_replace(buffer, &len, &changes, " der ", 5, " ", 1, 0);
+	str_replace(buffer, &len, &changes, " den ", 5, " ", 1, 0);
+	str_replace(buffer, &len, &changes, " die ", 5, " ", 1, 0);
+	str_replace(buffer, &len, &changes, " das ", 5, " ", 1, 0);
+	str_replace(buffer, &len, &changes, " la ", 4, " ", 1, 0);
+	str_replace(buffer, &len, &changes, " le ", 4, " ", 1, 0);
+	str_replace(buffer, &len, &changes, " el ", 4, " ", 1, 0);
+	str_replace(buffer, &len, &changes, " il ", 4, " ", 1, 0);
+
+	// german
+	str_replace(buffer, &len, &changes, "ae", 2, "a", 1, 0);
+	str_replace(buffer, &len, &changes, "oe", 2, "o", 1, 0);
+	str_replace(buffer, &len, &changes, "ue", 2, "u", 1, 0);
+	str_replace(buffer, &len, &changes, "sss", 3, "ss", 2, 0);
+	str_replace(buffer, &len, &changes, "ih", 2, "i", 1, 0);
+	str_replace(buffer, &len, &changes, "eh", 2, "e", 1, 0);
+
+	// russian
+	str_replace(buffer, &len, &changes, "ie", 2, "i", 1, 0);
+	str_replace(buffer, &len, &changes, "yi", 2, "i", 1, 0);
+
+	// allocate & create the result
+	len--;// Drop the terminating zero
+	result = (text *)palloc(len + VARHDRSZ);
+	SET_VARSIZE(result, len + VARHDRSZ);
+	memcpy(VARDATA(result), buffer, len);
+
+	pfree(buffer);
 
 	PG_RETURN_TEXT_P(result);
 }
