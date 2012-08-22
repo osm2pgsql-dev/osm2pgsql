19ce20990aad48bc7d1272187eca109d4445e292
Kai Krueger <kakrueger@gmail.com>
2012-08-22 18:40:48 +0000

    [osm2pgsql] Add a benchmark testing mode to node-persistent-cache-reader.
    node-persistent-cache-reader allows you to query the node-persistent-cache file by node id.
    
    This commit adds a benchmarking mode, in which it randomly requests nodes via the  persistent_cache_nodes_get_list,
    testing the read capabilities of the flat-node-cache code and measures the speed of retrieving nodes.

--- node-persistent-cache-reader.c
+++ node-persistent-cache-reader.c
@@ -8,6 +8,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <math.h>
+#include <sys/wait.h>
+#include <sys/time.h>
 
 #include "osmtypes.h"
 #include "output.h"
@@ -21,11 +23,42 @@ void exit_nicely()
     exit(1);
 }
 
+void test_get_node_list(int itterations, int max_size, int process_number) {
+    int i, node_cnt, node_cnt_total;
+    struct osmNode *nodes;
+    struct timeval start, stop;
+    struct timeval start_overall, stop_overall;
+    osmid_t *osmids;
+
+    node_cnt_total = 0;
+    gettimeofday(&start_overall, NULL);
+    for (i = 0; i < itterations; i++) {
+        node_cnt = random() % max_size;
+        node_cnt_total += node_cnt;
+
+        printf("Process %i: Getting %i nodes....\n", process_number, node_cnt);
+        nodes = malloc(sizeof(struct osmNode) * node_cnt);
+        osmids = malloc(sizeof(osmid_t) * node_cnt);
+        for (int j = 0; j < node_cnt; j++) {
+            osmids[j] = random() % (1 << 31);
+        }
+        gettimeofday(&start, NULL);
+        persistent_cache_nodes_get_list(nodes,osmids,node_cnt);
+        gettimeofday(&stop, NULL);
+        double duration = ((stop.tv_sec - start.tv_sec)*1000000.0 + (stop.tv_usec - start.tv_usec))/1000000.0;
+        printf("Process %i: Got nodes in %f at a rate of %f/s\n", process_number, duration, node_cnt / duration);
+    }
+    gettimeofday(&stop_overall, NULL);
+    double duration = ((stop_overall.tv_sec - start_overall.tv_sec)*1000000.0 + (stop_overall.tv_usec - start_overall.tv_usec))/1000000.0;
+    printf("Process %i: Got a total of nodes in %f at a rate of %f/s\n", process_number, duration, node_cnt_total / duration);
+}
+
 int main(int argc, char *argv[]) {
-	int i;
+	int i,p;
 	struct output_options options;
 	struct osmNode node;
 	struct osmNode *nodes;
+	struct timeval start, stop;
 	osmid_t *osmids;
 	int node_cnt;
 	options.append = 1;
@@ -33,9 +66,10 @@ int main(int argc, char *argv[]) {
 	options.flat_node_cache_enabled = 1;
 	options.flat_node_file = argv[1];
 	init_node_ram_cache(0,10,100);
-	init_node_persistent_cache(&options);
+
 
 	if (argc > 3) {
+	    init_node_persistent_cache(&options, 1, 1);
 		node_cnt = argc - 2;
 		nodes = malloc(sizeof(struct osmNode) * node_cnt);
 		osmids = malloc(sizeof(osmid_t) * node_cnt);
@@ -46,7 +80,53 @@ int main(int argc, char *argv[]) {
 		for (i = 0; i < node_cnt; i++) {
 			printf("lat: %f / lon: %f\n", nodes[i].lat, nodes[i].lon);
 		}
+	} else if (argc == 2) {
+        char state[128];
+        gettimeofday(&start, NULL);
+        initstate(start.tv_usec, &state, 8);
+        setstate(&state);
+
+	    printf("Testing mode\n");
+	    init_node_persistent_cache(&options, 1, 0);
+	    test_get_node_list(10, 200, 0);
+	    shutdown_node_persistent_cache();
+	    printf("Testing mode Async I/O\n");
+	    init_node_persistent_cache(&options, 1, 1);
+	    test_get_node_list(10, 200, 0);
+	    shutdown_node_persistent_cache();
+	    printf("Testing using multiple processes\n");
+	    int noProcs = 4;
+	    int pid;
+	    for (p = 1; p < noProcs; p++) {
+	        pid=fork();
+	        if (pid==0) {
+	            break;
+	        }
+	        if (pid==-1) {
+	            fprintf(stderr,"WARNING: Failed to fork helper processes. Falling back to only using %i \n", p);
+	            exit(1);
+	        }
+	    }
+	    gettimeofday(&start, NULL);
+	    initstate(start.tv_usec, &state, 8);
+	    setstate(&state);
+	    init_node_persistent_cache(&options, 1, 0);
+	    test_get_node_list(10,200,p);
+
+	    if (pid == 0) {
+	        shutdown_node_persistent_cache();
+	        fprintf(stderr,"Exiting process %i\n", p);
+	        exit(0);
+	    } else {
+	        //fprintf(stderr,"Counting %i\n", p);
+	        //for (long k = 0; k < (1l << 60); k++) if (k == (1l<<50)) printf(stderr,"SHouldn't be here");
+	        //fprintf(stderr,"Counted\n", p);
+
+	        for (p = 0; p < noProcs; p++) wait(NULL);
+	    }
+	    fprintf(stderr, "\nAll child processes exited\n");
 	} else {
+	    init_node_persistent_cache(&options, 1, 1);
 		if (strstr(argv[2],",") == NULL) {
 			persistent_cache_nodes_get(&node, atoi(argv[2]));
 			printf("lat: %f / lon: %f\n", node.lat, node.lon);
