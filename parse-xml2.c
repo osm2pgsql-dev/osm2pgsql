d8abc81d258b55f626c43938d0175e35cd9bcdb5
Matt Robinson <mwr54@nbcs.rutgers.edu>
2014-04-08 18:20:48 -0400

    Fixed crash related to missing lon/lat in deletions
    Previously if when attempting to delete a node osm2pgsql would crash if
    the lon/lat was not provided even though this data was not necessary.
    It will still crash if you are not trying to delete.

--- parse-xml2.c
+++ parse-xml2.c
@@ -100,12 +100,16 @@ static void StartElement(xmlTextReaderPtr reader, const xmlChar *name, struct os
         xid  = xmlTextReaderGetAttribute(reader, BAD_CAST "id");
         xlon = xmlTextReaderGetAttribute(reader, BAD_CAST "lon");
         xlat = xmlTextReaderGetAttribute(reader, BAD_CAST "lat");
-        assert(xid); assert(xlon); assert(xlat);
+        assert(xid);
 
         osmdata->osm_id   = strtoosmid((char *)xid, NULL, 10);
-        osmdata->node_lon = strtod((char *)xlon, NULL);
-        osmdata->node_lat = strtod((char *)xlat, NULL);
         osmdata->action   = ParseAction( reader , osmdata);
+
+        if (osmdata->action != ACTION_DELETE) {
+            assert(xlon); assert(xlat);
+            osmdata->node_lon = strtod((char *)xlon, NULL);
+            osmdata->node_lat = strtod((char *)xlat, NULL);
+        }
 
         if (osmdata->osm_id > osmdata->max_node)
             osmdata->max_node = osmdata->osm_id;
