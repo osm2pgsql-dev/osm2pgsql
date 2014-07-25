be269df8e0d2e2c6400bb3ef20a791cd7b2cedf5
Sam Haycraft <phreakdawg@gmail.com>
2014-07-25 15:50:55 -0600

    Added the capability to specify a target schema for the tables.

--- output.h
+++ output.h
@@ -51,6 +51,7 @@ struct output_options {
   int droptemp; /* drop slim mode temp tables after act */
   int unlogged; /* use unlogged tables where possible */
   int hstore_match_only; /* only copy rows that match an explicitly listed key */
+  char *schema; /* the target schema to use */
   int flat_node_cache_enabled;
   int excludepoly;
   const char *flat_node_file;
