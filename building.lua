b878898e9254e9583deb835b796fd8d60c582fcc
Kevin Kreiser <kevinkreiser@gmail.com>
2014-12-05 10:56:43 -0500

    add a sample multi backend confuration and sample lua script to use with it

---
+++ building.lua
@@ -0,0 +1,178 @@
+polygon_keys = { 'building', 'landuse', 'amenity', 'harbour', 'historic', 'leisure', 
+      'man_made', 'military', 'natural', 'office', 'place', 'power',
+      'public_transport', 'shop', 'sport', 'tourism', 'waterway',
+      'wetland', 'water', 'aeroway' }
+
+generic_keys = {'access','addr:housename','addr:housenumber','addr:interpolation','admin_level','aerialway','aeroway','amenity','area','barrier',
+   'bicycle','brand','bridge','boundary','building','capital','construction','covered','culvert','cutting','denomination','disused','ele',
+   'embarkment','foot','generation:source','harbour','highway','historic','hours','intermittent','junction','landuse','layer','leisure','lock',
+   'man_made','military','motor_car','name','natural','office','oneway','operator','place','poi','population','power','power_source','public_transport',
+   'railway','ref','religion','route','service','shop','sport','surface','toll','tourism','tower:type', 'tracktype','tunnel','water','waterway',
+   'wetland','width','wood','type'}
+
+function add_z_order(keyvalues)
+   z_order = 0
+   if (keyvalues["layer"] ~= nil and tonumber(keyvalues["layer"])) then
+      z_order = 10*keyvalues["layer"]
+   end
+
+   
+   zordering_tags = {{ 'railway', nil, 5, 1}, { 'boundary', 'administrative', 0, 1}, 
+      { 'bridge', 'yes', 10, 0 }, { 'bridge', 'true', 10, 0 }, { 'bridge', 1, 10, 0 },
+      { 'tunnel', 'yes', -10, 0}, { 'tunnel', 'true', -10, 0}, { 'tunnel', 1, -10, 0}, 
+      { 'highway', 'minor', 3, 0}, { 'highway', 'road', 3, 0 }, { 'highway', 'unclassified', 3, 0 },
+      { 'highway', 'residential', 3, 0 }, { 'highway', 'tertiary_link', 4, 0}, { 'highway', 'tertiary', 4, 0},
+      { 'highway', 'secondary_link', 6, 1}, { 'highway', 'secondary', 6, 1},
+      { 'highway', 'primary_link', 7, 1}, { 'highway', 'primary', 7, 1},
+      { 'highway', 'trunk_link', 8, 1}, { 'highway', 'trunk', 8, 1},
+      { 'highway', 'motorway_link', 9, 1}, { 'highway', 'motorway', 9, 1},
+}
+   
+   for i,k in ipairs(zordering_tags) do
+      if ((k[2]  and keyvalues[k[1]] == k[2]) or (k[2] == nil and keyvalues[k[1]] ~= nil)) then
+         if (k[4] == 1) then
+            roads = 1
+         end
+         z_order = z_order + k[3]
+      end
+   end
+
+   keyvalues["z_order"] = z_order
+
+   return keyvalues, roads
+
+end
+
+function filter_tags_generic(keyvalues, nokeys)
+   filter = 0
+   tagcount = 0
+
+   if nokeys == 0 then
+      filter = 1
+      return filter, keyvalues
+   end
+
+   delete_tags = { 'FIXME', 'note', 'source' }
+
+   for i,k in ipairs(delete_tags) do
+      keyvalues[k] = nil
+   end
+   
+   for k,v in pairs(keyvalues) do
+      for i, k2 in ipairs(generic_keys) do if k2 == k then tagcount = tagcount + 1; end end
+   end
+   if tagcount == 0 then
+      filter = 1
+   end
+
+   return filter, keyvalues
+end
+
+function nodes_proc (keyvalues, nokeys)
+   return filter_tags_generic(keyvalues, nokeys)
+end
+
+function rels_proc (keyvalues, nokeys)
+
+   filter, keyvalues = filter_tags_generic(keyvalues, nokeys)
+   if filter == 1 then
+      return filter, keyvalues
+   end
+
+   if ((keyvalues["type"] ~= "route") and (keyvalues["type"] ~= "multipolygon") and (keyvalues["type"] ~= "boundary")) then
+      filter = 1
+      return filter, keyvalues
+   end
+
+   return filter, keyvalues
+end
+
+function ways_proc (keyvalues, nokeys)
+   filter = 0
+   poly = 0
+   tagcount = 0
+   roads = 0
+
+   filter, keyvalues = filter_tags_generic(keyvalues, nokeys)
+   if filter == 1 then
+      return filter, keyvalues, poly, roads
+   end
+
+
+   for i,k in ipairs(polygon_keys) do
+      if keyvalues[k] then
+         poly=1
+         break
+      end
+   end
+   
+
+   if ((keyvalues["area"] == "yes") or (keyvalues["area"] == "1") or (keyvalues["area"] == "true")) then
+      poly = 1;
+   elseif ((keyvalues["area"] == "no") or (keyvalues["area"] == "0") or (keyvalues["area"] == "false")) then
+      poly = 0;
+   end
+
+   keyvalues, roads = add_z_order(keyvalues)
+
+
+   return filter, keyvalues, poly, roads
+end
+
+function rel_members_proc (keyvalues, keyvaluemembers, roles, membercount)
+   
+   filter = 0
+   boundary = 0
+   polygon = 0
+   roads = 0
+   membersuperseeded = {}
+   for i = 1, membercount do
+      membersuperseeded[i] = 0
+   end
+
+   type = keyvalues["type"]
+   keyvalues["type"] = nil
+  
+
+   if (type == "boundary") then
+      boundary = 1
+   end
+   if ((type == "multipolygon") and keyvalues["boundary"]) then
+      boundary = 1
+   elseif (type == "multipolygon") then
+      polygon = 1
+      polytagcount = 0;
+      for i,k in ipairs(polygon_keys) do
+         if keyvalues[k] then
+            polytagcount = polytagcount + 1
+         end
+      end
+      if (polytagcount == 0) then
+         for i = 1,membercount do
+            if (roles[i] == "outer") then
+               for k,v in pairs(keyvaluemembers[i]) do
+                  keyvalues[k] = v
+               end
+            end
+         end
+      end
+      for i = 1,membercount do
+         superseeded = 1
+         for k,v in pairs(keyvaluemembers[i]) do
+            if ((keyvalues[k] == nil) or (keyvalues[k] ~= v)) then
+               for j,k2 in ipairs(generic_keys) do
+                  if (k == k2) then
+                     superseeded = 0;
+                     break
+                  end
+               end
+            end
+         end
+         membersuperseeded[i] = superseeded
+      end
+   end
+
+   keyvalues, roads = add_z_order(keyvalues)
+
+   return filter, keyvalues, membersuperseeded, boundary, polygon, roads
+end
