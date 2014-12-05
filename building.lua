e33f21cc9bc80509993e7ec1a1551920723a4c70
Kevin Kreiser <kevinkreiser@gmail.com>
2014-12-05 16:12:25 -0500

    update building lua script to only care about buildings

--- building.lua
+++ building.lua
@@ -1,121 +1,64 @@
-polygon_keys = { 'building', 'landuse', 'amenity', 'harbour', 'historic', 'leisure', 
-      'man_made', 'military', 'natural', 'office', 'place', 'power',
-      'public_transport', 'shop', 'sport', 'tourism', 'waterway',
-      'wetland', 'water', 'aeroway' }
-
-generic_keys = {'access','addr:housename','addr:housenumber','addr:interpolation','admin_level','aerialway','aeroway','amenity','area','barrier',
-   'bicycle','brand','bridge','boundary','building','capital','construction','covered','culvert','cutting','denomination','disused','ele',
-   'embarkment','foot','generation:source','harbour','highway','historic','hours','intermittent','junction','landuse','layer','leisure','lock',
-   'man_made','military','motor_car','name','natural','office','oneway','operator','place','poi','population','power','power_source','public_transport',
-   'railway','ref','religion','route','service','shop','sport','surface','toll','tourism','tower:type', 'tracktype','tunnel','water','waterway',
-   'wetland','width','wood','type'}
-
-function add_z_order(keyvalues)
-   z_order = 0
-   if (keyvalues["layer"] ~= nil and tonumber(keyvalues["layer"])) then
-      z_order = 10*keyvalues["layer"]
-   end
-
-   
-   zordering_tags = {{ 'railway', nil, 5, 1}, { 'boundary', 'administrative', 0, 1}, 
-      { 'bridge', 'yes', 10, 0 }, { 'bridge', 'true', 10, 0 }, { 'bridge', 1, 10, 0 },
-      { 'tunnel', 'yes', -10, 0}, { 'tunnel', 'true', -10, 0}, { 'tunnel', 1, -10, 0}, 
-      { 'highway', 'minor', 3, 0}, { 'highway', 'road', 3, 0 }, { 'highway', 'unclassified', 3, 0 },
-      { 'highway', 'residential', 3, 0 }, { 'highway', 'tertiary_link', 4, 0}, { 'highway', 'tertiary', 4, 0},
-      { 'highway', 'secondary_link', 6, 1}, { 'highway', 'secondary', 6, 1},
-      { 'highway', 'primary_link', 7, 1}, { 'highway', 'primary', 7, 1},
-      { 'highway', 'trunk_link', 8, 1}, { 'highway', 'trunk', 8, 1},
-      { 'highway', 'motorway_link', 9, 1}, { 'highway', 'motorway', 9, 1},
-}
-   
-   for i,k in ipairs(zordering_tags) do
-      if ((k[2]  and keyvalues[k[1]] == k[2]) or (k[2] == nil and keyvalues[k[1]] ~= nil)) then
-         if (k[4] == 1) then
-            roads = 1
-         end
-         z_order = z_order + k[3]
-      end
-   end
-
-   keyvalues["z_order"] = z_order
-
-   return keyvalues, roads
-
-end
+tags = { 'building', 'shop', 'amenity' }
 
 function filter_tags_generic(keyvalues, nokeys)
    filter = 0
    tagcount = 0
 
+   --if there were no tags passed in, ie keyvalues is empty
    if nokeys == 0 then
       filter = 1
       return filter, keyvalues
    end
 
-   delete_tags = { 'FIXME', 'note', 'source' }
+   --remove anything we dont care about
+   for i,k in ipairs(tags) do
+      if keyvalues[k] then
+         tagcount = tagcount + 1
+      end
+   end
 
-   for i,k in ipairs(delete_tags) do
-      keyvalues[k] = nil
-   end
-   
-   for k,v in pairs(keyvalues) do
-      for i, k2 in ipairs(generic_keys) do if k2 == k then tagcount = tagcount + 1; end end
-   end
+   --if we didnt find any tags we care about
    if tagcount == 0 then
       filter = 1
    end
 
+   --tell the caller whether we think we want this feature or not and give back the modified tags
    return filter, keyvalues
 end
 
 function nodes_proc (keyvalues, nokeys)
-   return filter_tags_generic(keyvalues, nokeys)
+   --we dont care about nodes at all so filter all of them
+   filter = 1
+   return filter, keyvalues
 end
 
 function rels_proc (keyvalues, nokeys)
-
+   --let the generic filtering do its job
    filter, keyvalues = filter_tags_generic(keyvalues, nokeys)
    if filter == 1 then
       return filter, keyvalues
    end
 
-   if ((keyvalues["type"] ~= "route") and (keyvalues["type"] ~= "multipolygon") and (keyvalues["type"] ~= "boundary")) then
+   --dont keep any relations with types other than multipolygon
+   if keyvalues["type"] ~= "multipolygon" then
       filter = 1
       return filter, keyvalues
    end
 
+   --let the caller know if its a keeper or not and give back the modified tags
    return filter, keyvalues
 end
 
 function ways_proc (keyvalues, nokeys)
    filter = 0
-   poly = 0
-   tagcount = 0
+
+   --let the generic filtering do its job
+   filter, keyvalues = filter_tags_generic(keyvalues, nokeys)
+   poly = (filter + 1) % 2
    roads = 0
 
-   filter, keyvalues = filter_tags_generic(keyvalues, nokeys)
-   if filter == 1 then
-      return filter, keyvalues, poly, roads
-   end
-
-
-   for i,k in ipairs(polygon_keys) do
-      if keyvalues[k] then
-         poly=1
-         break
-      end
-   end
-   
-
-   if ((keyvalues["area"] == "yes") or (keyvalues["area"] == "1") or (keyvalues["area"] == "true")) then
-      poly = 1;
-   elseif ((keyvalues["area"] == "no") or (keyvalues["area"] == "0") or (keyvalues["area"] == "false")) then
-      poly = 0;
-   end
-
-   keyvalues, roads = add_z_order(keyvalues)
-
-
+   --let the caller know if its a keeper or not and give back the  modified tags
+   --also tell it whether or not its a polygon or road
    return filter, keyvalues, poly, roads
 end
 
@@ -125,54 +68,57 @@ function rel_members_proc (keyvalues, keyvaluemembers, roles, membercount)
    boundary = 0
    polygon = 0
    roads = 0
+
+   --mark each way of the relation to tell the caller if its going
+   --to be used in the relation or by itself as its own standalone way
+   --we start by assuming each way will not be used as part of the relation
    membersuperseeded = {}
    for i = 1, membercount do
       membersuperseeded[i] = 0
    end
 
+   --remember the type on the relation and erase it from the tags
    type = keyvalues["type"]
    keyvalues["type"] = nil
-  
 
-   if (type == "boundary") then
-      boundary = 1
-   end
-   if ((type == "multipolygon") and keyvalues["boundary"]) then
-      boundary = 1
-   elseif (type == "multipolygon") then
+   if (type == "multipolygon") and keyvalues["boundary"] == nil then
+      --check if this relation has tags we care about
       polygon = 1
-      polytagcount = 0;
-      for i,k in ipairs(polygon_keys) do
-         if keyvalues[k] then
-            polytagcount = polytagcount + 1
-         end
-      end
-      if (polytagcount == 0) then
+      filter, keyvalues = filter_tags_generic(keyvalues, 1)
+
+      --if the relation didn't have the tags we need go grab the tags from
+      --any members that are marked as outers of the multipolygon
+      if (filter == 1) then
          for i = 1,membercount do
             if (roles[i] == "outer") then
-               for k,v in pairs(keyvaluemembers[i]) do
-                  keyvalues[k] = v
+               for j,k in ipairs(tags) do
+                  v = keyvaluemembers[i][k]
+                  if v then
+                     keyvalues[k] = v
+                     filter = 0
+                  end
                end
             end
          end
       end
+      if filter == 1 then
+         return filter, keyvalues, membersuperseeded, boundary, polygon, roads
+      end
+
+      --for each tag of each member if the relation have the tag or has a non matching value for it
+      --then we say the member will not be used in the relation and is there for not superseeded
+      --ie it is kept as a standalone way 
       for i = 1,membercount do
          superseeded = 1
          for k,v in pairs(keyvaluemembers[i]) do
             if ((keyvalues[k] == nil) or (keyvalues[k] ~= v)) then
-               for j,k2 in ipairs(generic_keys) do
-                  if (k == k2) then
-                     superseeded = 0;
-                     break
-                  end
-               end
+                superseeded = 0;
+                break
             end
          end
          membersuperseeded[i] = superseeded
       end
    end
-
-   keyvalues, roads = add_z_order(keyvalues)
 
    return filter, keyvalues, membersuperseeded, boundary, polygon, roads
 end
