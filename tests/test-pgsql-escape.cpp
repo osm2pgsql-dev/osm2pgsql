d4a6a65ddd4c01ba5e73193925947ddf18ff4be8
Sarah Hoffmann <lonvia@denofr.de>
2014-12-22 11:21:40 +0100

    remove unnecessary strlen() and provide more tests for escape()

--- tests/test-pgsql-escape.cpp
+++ tests/test-pgsql-escape.cpp
@@ -1,7 +1,21 @@
 #include "pgsql.hpp"
 
+void test_escape(const char *in, const char *out) {
+    std::string sql;
+    escape(in, sql);
+    if (sql.compare(out) != 0) {
+        std::cerr << "Expected " << out << ", but got " << sql << " for " << in <<".\n";
+        exit(1);
+    }
+}
+
 int main(int argc, char *argv[]) {
     std::string sql;
-    escape("farmland", sql);
-    return sql.compare("farmland") != 0;
+    test_escape("farmland", "farmland");
+    test_escape("", "");
+    test_escape("\\", "\\\\");
+    test_escape("foo\nbar", "foo\\\nbar");
+    test_escape("\t\r\n", "\\\t\\\r\\\n");
+
+    return 0;
 }
