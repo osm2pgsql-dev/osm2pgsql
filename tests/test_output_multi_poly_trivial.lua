1f1d4e9f9c64f259689566456ce1f7b684ab290e
Matt Amos <zerebubuth@gmail.com>
2015-04-20 18:56:48 +0100

    Added test case for multi-backend and polygons from relations.
    This seems to be a case which wasn't tested in isolation before,
    so it's hard to tell what the expected behaviour should be. The
    tested behaviour is that the tags on the output row(s) should be
    processed through _both_ the filters for the relation and the
    members.

---
+++ tests/test_output_multi_poly_trivial.lua
@@ -0,0 +1,23 @@
+function drop_all (...)
+  return 1, {}
+end
+
+function drop_ways (...)
+  return 1, {}, 0, 0
+end
+
+function test_rels (kv, num_keys)
+  tags = {["foo"] = "bar"}
+  return 0, tags
+end
+
+function test_members (kv, member_tags, roles, num_members)
+  membersuperseeded = {}
+  for i = 1, num_members do
+    membersuperseeded[i] = 0
+  end
+
+  tags = kv
+
+  return 0, tags, membersuperseeded, 0, 0, 0
+end
