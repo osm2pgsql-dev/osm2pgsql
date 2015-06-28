0f1ca7d13e8876c6c0cfc05bba99a68c89a99449
Paul Norman <penorman@mac.com>
2015-06-28 16:41:47 -0700

    Cleanup test-middle-ram

--- tests/test-middle-ram.cpp
+++ tests/test-middle-ram.cpp
@@ -16,33 +16,29 @@ int main(int argc, char *argv[]) {
   try {
     options_t options;
     options.scale = 10000000;
-    options.alloc_chunkwise = ALLOC_SPARSE | ALLOC_DENSE;
-    options.cache = 1;
+    options.cache = 1; // Non-zero cache is needed to test
 
-    middle_ram_t mid_ram;
-    output_null_t out_test(&mid_ram, options);
+    {
+      options.alloc_chunkwise = ALLOC_SPARSE | ALLOC_DENSE; // optimized
+      middle_ram_t mid_ram;
+      output_null_t out_test(&mid_ram, options);
 
-    mid_ram.start(&options);
+      mid_ram.start(&options);
 
-    int status = 0;
+      if (test_node_set(&mid_ram) != 0) { throw std::runtime_error("test_node_set failed with optimized cache."); }
 
-    status = test_node_set(&mid_ram);
-    if (status != 0) { throw std::runtime_error("test_node_set failed."); }
+      if (test_way_set(&mid_ram) != 0) { throw std::runtime_error("test_way_set failed with optimized cache."); }
 
-    status = test_way_set(&mid_ram);
-    if (status != 0) { throw std::runtime_error("test_way_set failed."); }
-
-    mid_ram.commit();
-    mid_ram.stop();
-
-    return 0;
-
+      mid_ram.commit();
+      mid_ram.stop();
+    }
   } catch (const std::exception &e) {
     std::cerr << "ERROR: " << e.what() << std::endl;
-
+    return 1;
   } catch (...) {
     std::cerr << "UNKNOWN ERROR" << std::endl;
+    return 1;
   }
 
-  return 1;
+  return 0;
 }
