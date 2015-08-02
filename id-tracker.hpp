4a667fba6f95bd21c442e2f90cced000c0717164
Paul Norman <penorman@mac.com>
2015-08-02 00:58:55 -0700

    Add documentation to id_tracker
    This is based on @zerebubuth's comments in
    https://github.com/openstreetmap/osm2pgsql/commit/aa1ff6e5d07b7a356bc88093d2673fc7929c2133#commitcomment-10961958

--- id-tracker.hpp
+++ id-tracker.hpp
@@ -5,12 +5,29 @@
 #include <boost/noncopyable.hpp>
 #include <memory>
 
+/**
+  * Tracker for if an element needs to be revisited later in the process, also
+  * known as "pending". This information used to be stored in the database, but
+  * is ephemeral and lead to database churn, bloat, and was generally slow.
+  * An initial re-implementation stored it as a std::set<osmid_t>, which worked
+  * but was inefficient with memory overhead and pointer chasing.
+  *
+  * Instead, the size of the leaf nodes is increased. This was initially a
+  * vector<bool>, but the cost of exposing the iterator was too high.
+  * Instead, it's a uint32, with a function to find the next bit set in the block
+  *
+  * These details aren't exposed in the public interface, which just has
+  * pop_mark.
+  */
 struct id_tracker : public boost::noncopyable {
     id_tracker();
     ~id_tracker();
 
     void mark(osmid_t id);
     bool is_marked(osmid_t id);
+    /**
+     * Finds an osmid_t that is marked
+     */
     osmid_t pop_mark();
     size_t size();
     osmid_t last_returned() const;
