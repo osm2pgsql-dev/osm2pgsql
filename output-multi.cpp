8ffe16e67da43fd531a5e73b10d31465324f4a91
Paul Norman <penorman@mac.com>
2017-04-12 12:55:26 -0700

    Use the same logic for queuing pending ways with multi and pgsql
    (cherry picked from commit 323a43dc2ae06b27a0bdad9968723a694f02363b)

--- output-multi.cpp
+++ output-multi.cpp
@@ -92,7 +92,7 @@ void output_multi_t::enqueue_ways(pending_queue_t &job_queue, osmid_t id, size_t
     }
 
     //make sure to get this one as well and move to the next
-    if(popped == id) {
+    if (popped > id) {
         if (!ways_done_tracker->is_marked(popped) && id_tracker::is_valid(popped)) {
             job_queue.push(pending_job_t(popped, output_id));
             added++;
@@ -143,7 +143,7 @@ void output_multi_t::enqueue_relations(pending_queue_t &job_queue, osmid_t id, s
     }
 
     //make sure to get this one as well and move to the next
-    if(popped == id) {
+    if (popped > id) {
         if(id_tracker::is_valid(popped)) {
             job_queue.push(pending_job_t(popped, output_id));
             added++;
