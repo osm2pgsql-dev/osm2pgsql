8b9dea0b26a167f4fa71e844ef8da61500bef86b
ImreSamu <ImreSamu@users.noreply.github.com>
2018-06-07 22:19:35 +0200

    add documentations, other db test, fixes

--- docker/travis_test.sh
+++ docker/travis_test.sh
@@ -8,12 +8,22 @@ docker-compose -v
 # Travis Docker is sensitive - so "build" must exist!
 mkdir -p build
 
+
+# input parameters !  should fail if not exist! 
+echo "------------------------------------"
+echo " ----        testing!          -----"
+echo "------------------------------------"
+echo "GCCVER           : $GCCVER"
+echo "CXXFLAGS         : $CXXFLAGS"
+echo "LUA_OPTION       : $LUA_OPTION"
+echo "LUAJIT_OPTION    : $LUAJIT_OPTION"
+echo "RUNTEST (filter) : $RUNTEST"
+echo "------------------------------------"
+
+
 # Down- any running process ( for example local testing  ) 
 docker-compose down
 
-echo "------------------------------------"
-echo " ----  testing with gcc$GCCVER -----"
-echo "------------------------------------"
 
 ./docker/docker_build_gcc_image.sh
 docker-compose run --rm osm2pgsql-dev ../docker/info.sh
