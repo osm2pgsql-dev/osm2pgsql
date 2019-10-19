709665022064498efb23f34d4379f9b0b025b2e4
Sarah Hoffmann <lonvia@denofr.de>
2019-10-19 23:34:42 +0200

    move tablespace test to catch

---
+++ newtests/test-output-pgsql-tablespace.cpp
@@ -0,0 +1,36 @@
+#include "catch.hpp"
+
+#include "common-import.hpp"
+#include "configs.hpp"
+
+static testing::db::import_t db;
+
+static void require_tables(pg::conn_t const &conn)
+{
+    conn.require_has_table("osm2pgsql_test_point");
+    conn.require_has_table("osm2pgsql_test_line");
+    conn.require_has_table("osm2pgsql_test_polygon");
+    conn.require_has_table("osm2pgsql_test_roads");
+}
+
+TEST_CASE("simple import with tables spaces")
+{
+    {
+        auto conn = db.db().connect();
+        REQUIRE(1 == conn.get_count("pg_tablespace", "spcname = 'tablespacetest'"));
+    }
+
+    testing::options::slim_default options(db.db());
+    options.tblsslim_index = "tablespacetest";
+    options.tblsslim_data = "tablespacetest";
+
+    REQUIRE_NOTHROW(db.run_file(options, "liechtenstein-2013-08-03.osm.pbf"));
+
+    auto conn = db.db().connect();
+    require_tables(conn);
+
+    REQUIRE(1342 == conn.get_count("osm2pgsql_test_point"));
+    REQUIRE(3231 == conn.get_count("osm2pgsql_test_line"));
+    REQUIRE(375  == conn.get_count("osm2pgsql_test_roads"));
+    REQUIRE(4130 == conn.get_count("osm2pgsql_test_polygon"));
+}
