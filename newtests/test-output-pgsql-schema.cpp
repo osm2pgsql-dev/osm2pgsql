8cd48cb50611c3fa0280a0799d6730a4dcf3daec
Sarah Hoffmann <lonvia@denofr.de>
2019-10-19 23:19:39 +0200

    port schema tests to catch

---
+++ newtests/test-output-pgsql-schema.cpp
@@ -0,0 +1,42 @@
+#include "catch.hpp"
+
+#include "common-import.hpp"
+#include "configs.hpp"
+
+static testing::db::import_t db;
+
+TEST_CASE("schema separation")
+{
+    {
+        auto conn = db.db().connect();
+        conn.exec("CREATE SCHEMA myschema;"
+                  "CREATE TABLE myschema.osm2pgsql_test_point (id bigint);"
+                  "CREATE TABLE myschema.osm2pgsql_test_line (id bigint);"
+                  "CREATE TABLE myschema.osm2pgsql_test_polygon (id bigint);"
+                  "CREATE TABLE myschema.osm2pgsql_test_roads (id bigint)");
+    }
+
+    testing::options::slim_default options(db.db());
+
+    REQUIRE_NOTHROW(db.run_file(options, "test_output_pgsql_z_order.osm"));
+
+    auto conn = db.db().connect();
+
+    conn.require_has_table("public.osm2pgsql_test_point");
+    conn.require_has_table("public.osm2pgsql_test_line");
+    conn.require_has_table("public.osm2pgsql_test_polygon");
+    conn.require_has_table("public.osm2pgsql_test_roads");
+    conn.require_has_table("myschema.osm2pgsql_test_point");
+    conn.require_has_table("myschema.osm2pgsql_test_line");
+    conn.require_has_table("myschema.osm2pgsql_test_polygon");
+    conn.require_has_table("myschema.osm2pgsql_test_roads");
+
+    REQUIRE(2 == conn.get_count("osm2pgsql_test_point"));
+    REQUIRE(11 == conn.get_count("osm2pgsql_test_line"));
+    REQUIRE(1 == conn.get_count("osm2pgsql_test_polygon"));
+    REQUIRE(8 == conn.get_count("osm2pgsql_test_roads"));
+    REQUIRE(0 == conn.get_count("myschema.osm2pgsql_test_point"));
+    REQUIRE(0 == conn.get_count("myschema.osm2pgsql_test_line"));
+    REQUIRE(0 == conn.get_count("myschema.osm2pgsql_test_polygon"));
+    REQUIRE(0 == conn.get_count("myschema.osm2pgsql_test_roads"));
+}
