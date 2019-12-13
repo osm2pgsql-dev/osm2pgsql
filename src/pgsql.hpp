695337a74883092f3ce588e44fe5aca3644a41e5
Jochen Topf <jochen@topf.org>
2019-12-13 10:43:30 +0100

    Cleanup of pgsql classes

--- src/pgsql.hpp
+++ src/pgsql.hpp
@@ -9,17 +9,15 @@
 #include <memory>
 #include <string>
 
-struct pg_result_deleter_t
-{
-    void operator()(PGresult *p) const noexcept { PQclear(p); }
-};
-
-using pg_result_wrapper_t = std::unique_ptr<PGresult, pg_result_deleter_t>;
-
+/**
+ * PostgreSQL query result.
+ *
+ * Wraps the PGresult object of the libpq library.
+ */
 class pg_result_t
 {
 public:
-    pg_result_t(PGresult *result) : m_result(result) {}
+    explicit pg_result_t(PGresult *result) noexcept : m_result(result) {}
 
     /// Get a pointer to the underlying PGresult object.
     PGresult *get() const noexcept { return m_result.get(); }
@@ -70,32 +68,29 @@ public:
     }
 
 private:
-    pg_result_wrapper_t m_result;
-};
+    struct pg_result_deleter_t
+    {
+        void operator()(PGresult *p) const noexcept { PQclear(p); }
+    };
 
-struct pg_conn_deleter_t
-{
-    void operator()(PGconn *p) const noexcept { PQfinish(p); }
+    std::unique_ptr<PGresult, pg_result_deleter_t> m_result;
 };
-
-using pg_conn_wrapper_t = std::unique_ptr<PGconn, pg_conn_deleter_t>;
 
 /**
- * Simple postgres connection.
+ * PostgreSQL connection.
+ *
+ * Wraps the PGconn object of the libpq library.
  *
  * The connection is automatically closed when the object is destroyed.
  */
 class pg_conn_t
 {
 public:
-    pg_conn_t(std::string const &connection);
+    explicit pg_conn_t(std::string const &conninfo);
 
     pg_result_t exec_prepared(char const *stmt, int num_params,
                               const char *const *param_values,
                               ExecStatusType expect = PGRES_TUPLES_OK) const;
-
-    void copy_data(std::string const &sql, std::string const &context) const;
-    void end_copy(std::string const &context) const;
 
     pg_result_t query(ExecStatusType expect, char const *sql) const;
 
@@ -105,10 +100,19 @@ public:
 
     void exec(std::string const &sql) const;
 
+    void copy_data(std::string const &sql, std::string const &context) const;
+
+    void end_copy(std::string const &context) const;
+
     char const *error_msg() const noexcept;
 
 private:
-    pg_conn_wrapper_t m_conn;
+    struct pg_conn_deleter_t
+    {
+        void operator()(PGconn *p) const noexcept { PQfinish(p); }
+    };
+
+    std::unique_ptr<PGconn, pg_conn_deleter_t> m_conn;
 };
 
 #endif // OSM2PGSQL_PGSQL_HPP
