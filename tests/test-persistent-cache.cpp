2464d283282e8fc834fd23aa175285f61fb4b4c4
Jochen Topf <jochen@topf.org>
2020-04-07 15:22:54 +0200

    Add default constructor for node_ram_cache for testing

--- tests/test-persistent-cache.cpp
+++ tests/test-persistent-cache.cpp
@@ -29,7 +29,7 @@ TEST_CASE("Persistent cache", "[NoDB]")
 {
     options_t const options = testing::opt_t().flatnodes();
     testing::cleanup::file_t flatnode_cleaner{options.flat_node_file.get()};
-    auto ram_cache = std::make_shared<node_ram_cache>(0, 0); // empty cache
+    auto ram_cache = std::make_shared<node_ram_cache>(); // dummy cache
 
     // create a new cache
     {
