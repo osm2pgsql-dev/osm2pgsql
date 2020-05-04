219e0433ebc1526e43a283fb580dfbf60b80b27d
mmd-osm <mmd.osm@gmail.com>
2020-05-04 19:56:31 +0200

    luaL_traceback is not supported in Lua 5.1, except for LuaJIT

--- src/lua-utils.cpp
+++ src/lua-utils.cpp
@@ -1,3 +1,4 @@
+#include "config.h"
 #include "lua-utils.hpp"
 #include "format.hpp"
 
@@ -158,11 +159,16 @@ static int pcall_error_traceback_handler(lua_State *lua_state)
 // wrapper function for lua_pcall to include a stack traceback
 int luaX_pcall(lua_State *lua_state, int narg, int nres)
 {
+// Lua 5.1 doesn't support luaL_traceback, unless LuaJIT is used
+#if LUA_VERSION_NUM < 502 && !defined(HAVE_LUAJIT)
+    int const status = lua_pcall(lua_state, narg, nres, 0);
+#else
     int const base = lua_gettop(lua_state) - narg; // function index
     lua_pushcfunction(lua_state,
                       pcall_error_traceback_handler); // push message handler
     lua_insert(lua_state, base); // put it under function and args
     int const status = lua_pcall(lua_state, narg, nres, base);
     lua_remove(lua_state, base); // remove message handler from the stack
+#endif
     return status;
 }
