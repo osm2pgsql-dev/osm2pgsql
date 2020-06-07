e81aa400d19892c7599efbdea4b234db43cd95b0
Jochen Topf <jochen@topf.org>
2020-06-07 11:27:00 +0200

    Refactor projection code
    Move away coordinates handling from functions with separate lon/lat
    parameters to use of osmium::geom::Coordinates. This also removes in/out
    parameters from transformation functions.
    
    This also moves the reprojection::coords_to_tile() function as a free
    function into expire-tiles.cpp, because it is only used and fits better
    there. (The function is not dependent on the type of projection.)
    Further refactoring is needed in the expire-tiles.cpp code.

--- src/expire-tiles.cpp
+++ src/expire-tiles.cpp
@@ -122,6 +122,21 @@ int expire_tiles::normalise_tile_x_coord(int x)
     return x;
 }
 
+/**
+ * Converts from target coordinates to tile coordinates.
+ *
+ * The zoom level for the coordinates is explicitly given in the
+ * parameter map_width.
+ */
+static void coords_to_tile(reprojection const &projection, double *tilex,
+                           double *tiley, double lon, double lat, int map_width)
+{
+    auto const c = projection.target_to_tile(Coordinates{lon, lat});
+
+    *tilex = map_width * (0.5 + c.x / EARTH_CIRCUMFERENCE);
+    *tiley = map_width * (0.5 - c.y / EARTH_CIRCUMFERENCE);
+}
+
 /*
  * Expire tiles that a line crosses
  */
@@ -133,8 +148,8 @@ void expire_tiles::from_line(double lon_a, double lat_a, double lon_b,
     double tile_x_b;
     double tile_y_b;
 
-    projection->coords_to_tile(&tile_x_a, &tile_y_a, lon_a, lat_a, map_width);
-    projection->coords_to_tile(&tile_x_b, &tile_y_b, lon_b, lat_b, map_width);
+    coords_to_tile(*projection, &tile_x_a, &tile_y_a, lon_a, lat_a, map_width);
+    coords_to_tile(*projection, &tile_x_b, &tile_y_b, lon_b, lat_b, map_width);
 
     if (tile_x_a > tile_x_b) {
         /* We always want the line to go from left to right - swap the ends if it doesn't */
@@ -224,10 +239,10 @@ int expire_tiles::from_bbox(double min_lon, double min_lat, double max_lon,
     /* Convert the box's Mercator coordinates into tile coordinates */
     double tmp_x;
     double tmp_y;
-    projection->coords_to_tile(&tmp_x, &tmp_y, min_lon, max_lat, map_width);
+    coords_to_tile(*projection, &tmp_x, &tmp_y, min_lon, max_lat, map_width);
     int min_tile_x = tmp_x - TILE_EXPIRY_LEEWAY;
     int min_tile_y = tmp_y - TILE_EXPIRY_LEEWAY;
-    projection->coords_to_tile(&tmp_x, &tmp_y, max_lon, min_lat, map_width);
+    coords_to_tile(*projection, &tmp_x, &tmp_y, max_lon, min_lat, map_width);
     int max_tile_x = tmp_x + TILE_EXPIRY_LEEWAY;
     int max_tile_y = tmp_y + TILE_EXPIRY_LEEWAY;
     if (min_tile_x < 0) {
--- src/reprojection.hpp
+++ src/reprojection.hpp
@@ -1,10 +1,12 @@
 #ifndef OSM2PGSQL_REPROJECTION_HPP
 #define OSM2PGSQL_REPROJECTION_HPP
 
-/*
- * Convert OSM latitude / longitude from degrees to mercator
- * so that Mapnik does not have to project the data again
+/**
+ * \file
  *
+ * This file is part of osm2pgsql (https://github.com/openstreetmap/osm2pgsql).
+ *
+ * It contains the reprojection class.
  */
 
 #include <memory>
@@ -12,12 +14,20 @@
 #include <osmium/geom/coordinates.hpp>
 #include <osmium/osm/location.hpp>
 
+using Coordinates = osmium::geom::Coordinates;
+
 enum Projection
 {
     PROJ_LATLONG = 4326,
     PROJ_SPHERE_MERC = 3857
 };
 
+/**
+ * Virtual base class used for projecting OSM WGS84 coordinates into a
+ * different coordinate system. Most commonly this will be used to convert
+ * the coordinates into Spherical Mercator coordinates used in common web
+ * tiles.
+ */
 class reprojection
 {
 public:
@@ -35,28 +45,22 @@ public:
      * Reproject from the source projection lat/lon (EPSG:4326)
      * to target projection.
      */
-    virtual osmium::geom::Coordinates reproject(osmium::Location loc) const = 0;
+    virtual Coordinates reproject(osmium::Location loc) const = 0;
 
     /**
-     * Converts coordinates from target projection to tile projection (EPSG:3857)
+     * Converts coordinates from target projection to tile projection
+     * (EPSG:3857)
      *
-     * Do not confuse with coords_to_tile which does *not* calculate coordinates in the
-     * tile projection, but tile coordinates.
+     * Do not confuse with coords_to_tile which does *not* calculate
+     * coordinates in the tile projection, but tile coordinates.
      */
-    virtual void target_to_tile(double *lat, double *lon) const = 0;
+    virtual Coordinates target_to_tile(Coordinates) const = 0;
 
-    /**
-     * Converts from target coordinates to tile coordinates.
-     *
-     * The zoom level for the coordinates is explicitly given in the
-     * variable map_width.
-     */
-    void coords_to_tile(double *tilex, double *tiley, double lon, double lat,
-                        int map_width);
-    virtual int target_srs() const = 0;
-    virtual char const *target_desc() const = 0;
+    virtual int target_srs() const noexcept = 0;
 
-    bool target_latlon() const { return target_srs() == PROJ_LATLONG; }
+    virtual char const *target_desc() const noexcept = 0;
+
+    bool target_latlon() const noexcept { return target_srs() == PROJ_LATLONG; }
 
     /**
      * Create a reprojection object with target srs `srs`.
