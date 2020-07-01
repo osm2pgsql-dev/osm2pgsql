33fb1c8c58a0068ddffb8bd9c7cb2de3f2c05585
Jochen Topf <jochen@topf.org>
2020-07-01 10:21:31 +0200

    Cleanup and extend flex invalid geometry test

---
+++ tests/data/test_output_flex_invalid_geom.lua
@@ -0,0 +1,45 @@
+
+local tables = {}
+
+tables.line = osm2pgsql.define_table{
+    name = 'osm2pgsql_test_line',
+    ids = { type = 'way', id_column = 'osm_id' },
+    columns = {
+        { column = 'tags', type = 'hstore' },
+        { column = 'geom', type = 'linestring' },
+    }
+}
+
+tables.polygon = osm2pgsql.define_table{
+    name = 'osm2pgsql_test_polygon',
+    ids = { type = 'area', id_column = 'osm_id' },
+    columns = {
+        { column = 'tags', type = 'hstore' },
+        { column = 'geom', type = 'geometry' }
+    }
+}
+
+function osm2pgsql.process_way(object)
+    if not next(object.tags) then
+        return
+    end
+
+    if object.tags.natural then
+        tables.polygon:add_row({
+            tags = object.tags,
+            geom = { create = 'area' }
+        })
+    else
+        tables.line:add_row({
+            tags = object.tags
+        })
+    end
+end
+
+function osm2pgsql.process_relation(object)
+    tables.polygon:add_row({
+        tags = object.tags,
+        geom = { create = 'area', multi = false }
+    })
+end
+
