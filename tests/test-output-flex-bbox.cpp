44b12019a9e645dfa79f92deaad637759ca6a14d
Jochen Topf <jochen@topf.org>
2020-07-01 09:58:11 +0200

    Extend bbox test to mercator projection

--- tests/test-output-flex-bbox.cpp
+++ tests/test-output-flex-bbox.cpp
@@ -5,11 +5,12 @@
 
 static testing::db::import_t db;
 
-TEST_CASE("bbox on nodes and ways")
+static char const *const conf_file = "test_output_flex_bbox.lua";
+
+TEST_CASE("bbox on nodes and ways in 4326")
 {
-    testing::opt_t options = testing::opt_t()
-                                 .flex("test_output_flex_bbox.lua")
-                                 .srs(PROJ_LATLONG);
+    options_t const options =
+        testing::opt_t().flex(conf_file).srs(PROJ_LATLONG);
 
     REQUIRE_NOTHROW(db.run_import(options,
                                   "n10 v1 dV Ta=b x10.0 y10.0\n"
@@ -41,3 +42,29 @@ TEST_CASE("bbox on nodes and ways")
                    "abs(max_x - 10.2) < 0.01 AND abs(max_y - 10.2) < 0.01"));
 }
 
+TEST_CASE("bbox on nodes and ways in 3857")
+{
+    options_t const options = testing::opt_t().flex(conf_file);
+
+    REQUIRE_NOTHROW(db.run_import(options,
+                                  "n10 v1 dV Ta=b x10.0 y10.0\n"
+                                  "n11 v1 dV Ta=b x10.0 y10.2\n"
+                                  "n12 v1 dV Ta=b x10.2 y10.2\n"
+                                  "w20 v1 dV Thighway=primary Nn10,n11,n12\n"));
+
+    auto conn = db.db().connect();
+
+    CHECK(3 == conn.get_count("osm2pgsql_test_points"));
+
+    CHECK(1 == conn.get_count(
+                   "osm2pgsql_test_points",
+                   "abs(min_x - 10.0) < 0.01 AND abs(min_y - 10.0) < 0.01 AND "
+                   "abs(max_x - 10.0) < 0.01 AND abs(max_y - 10.0) < 0.01"));
+
+    CHECK(1 == conn.get_count("osm2pgsql_test_highways"));
+
+    CHECK(1 == conn.get_count(
+                   "osm2pgsql_test_highways",
+                   "abs(min_x - 10.0) < 0.01 AND abs(min_y - 10.0) < 0.01 AND "
+                   "abs(max_x - 10.2) < 0.01 AND abs(max_y - 10.2) < 0.01"));
+}
