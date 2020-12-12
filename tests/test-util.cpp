dd36e43c729749e924bf5949e35473d784f18ae3
Jochen Topf <jochen@topf.org>
2020-12-12 15:17:18 +0100

    Mark more tests as NoDB

--- tests/test-util.cpp
+++ tests/test-util.cpp
@@ -5,37 +5,37 @@
 #include <cstring>
 #include <limits>
 
-TEST_CASE("integer_to_buffer 1")
+TEST_CASE("integer_to_buffer 1", "[NoDB]")
 {
     util::integer_to_buffer buffer{1};
     REQUIRE(std::strcmp(buffer.c_str(), "1") == 0);
 }
 
-TEST_CASE("integer_to_buffer max")
+TEST_CASE("integer_to_buffer max", "[NoDB]")
 {
     util::integer_to_buffer buffer{std::numeric_limits<osmid_t>::max()};
     REQUIRE(std::strcmp(buffer.c_str(), "9223372036854775807") == 0);
 }
 
-TEST_CASE("integer_to_buffer min")
+TEST_CASE("integer_to_buffer min", "[NoDB]")
 {
     util::integer_to_buffer buffer{std::numeric_limits<osmid_t>::min()};
     REQUIRE(std::strcmp(buffer.c_str(), "-9223372036854775808") == 0);
 }
 
-TEST_CASE("double_to_buffer 0")
+TEST_CASE("double_to_buffer 0", "[NoDB]")
 {
     util::double_to_buffer buffer{0.0};
     REQUIRE(std::strcmp(buffer.c_str(), "0") == 0);
 }
 
-TEST_CASE("double_to_buffer 3.141")
+TEST_CASE("double_to_buffer 3.141", "[NoDB]")
 {
     util::double_to_buffer buffer{3.141};
     REQUIRE(std::strcmp(buffer.c_str(), "3.141") == 0);
 }
 
-TEST_CASE("human readable time durations")
+TEST_CASE("human readable time durations", "[NoDB]")
 {
     REQUIRE(util::human_readable_duration(0) == "0s");
     REQUIRE(util::human_readable_duration(17) == "17s");
