05a744806e7917aa52d4108cd589a25acb42d8c2
Jochen Topf <jochen@topf.org>
2022-01-19 11:24:28 +0100

    Update to newest libosmium version 2.17.3

--- contrib/libosmium/include/osmium/version.hpp
+++ contrib/libosmium/include/osmium/version.hpp
@@ -5,7 +5,7 @@
 
 This file is part of Osmium (https://osmcode.org/libosmium).
 
-Copyright 2013-2021 Jochen Topf <jochen@topf.org> and others (see README).
+Copyright 2013-2022 Jochen Topf <jochen@topf.org> and others (see README).
 
 Boost Software License - Version 1.0 - August 17th, 2003
 
@@ -35,8 +35,8 @@ DEALINGS IN THE SOFTWARE.
 
 #define LIBOSMIUM_VERSION_MAJOR 2
 #define LIBOSMIUM_VERSION_MINOR 17
-#define LIBOSMIUM_VERSION_PATCH 2
+#define LIBOSMIUM_VERSION_PATCH 3
 
-#define LIBOSMIUM_VERSION_STRING "2.17.2"
+#define LIBOSMIUM_VERSION_STRING "2.17.3"
 
 #endif // OSMIUM_VERSION_HPP
