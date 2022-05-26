909005d033861ad0aeac35b8e4d13599936c36ce
Sarah Hoffmann <lonvia@denofr.de>
2022-05-26 21:53:53 +0200

    convert test-output-flex-bbox to BDD tests
    Requires a new geometry comparison type. This is done by expecting
    WKT from Postgres (via ST_AsText()) and comparing it against a
    simplified geometry description in the feature file.

--- tests/bdd/steps/steps_db.py
+++ tests/bdd/steps/steps_db.py
@@ -8,6 +8,7 @@
 Steps that query the database.
 """
 import math
+import re
 from typing import Iterable
 
 from psycopg2 import sql
@@ -73,7 +74,7 @@ def db_check_table_content(context, table):
     with context.db.cursor() as cur:
         cur.execute(sql.SQL("SELECT {} FROM {}").format(rows, sql.Identifier(table)))
 
-        actuals = list(DBRow(r, context.table.headings) for r in cur)
+        actuals = list(DBRow(r, context.table.headings, context.geometry_factory) for r in cur)
 
     linenr = 1
     for row in context.table.rows:
@@ -86,7 +87,7 @@ def db_check_sql_statement(context, query):
     with context.db.cursor() as cur:
         cur.execute(query)
 
-        actuals = list(DBRow(r, context.table.headings) for r in cur)
+        actuals = list(DBRow(r, context.table.headings, context.geometry_factory) for r in cur)
 
     linenr = 1
     for row in context.table.rows:
@@ -119,7 +120,7 @@ def table_exists(conn, table):
 
 class DBRow:
 
-    def __init__(self, row, headings):
+    def __init__(self, row, headings, factory):
         self.data = []
         for value, head in zip(row, headings):
             if '@' in head:
@@ -131,6 +132,8 @@ class DBRow:
                 self.data.append(DBValueFloat(value, props))
             elif value is None:
                 self.data.append(None)
+            elif head.lower().startswith('st_astext('):
+                self.data.append(DBValueGeometry(value, props, factory))
             else:
                 self.data.append(str(value))
 
@@ -143,6 +146,57 @@ class DBRow:
 
     def __repr__(self):
         return '\n[' + ', '.join(str(s) for s in self.data) + ']'
+
+
+class DBValueGeometry:
+
+    def __init__(self, value, props, factory):
+        self.precision = float(props) if props else 0.01
+        self.orig_value = value
+        self.set_coordinates(value)
+        self.factory = factory
+
+    def set_coordinates(self, value):
+        m = re.fullmatch(r'(POINT|LINESTRING|POLYGON)\((.*)\)', value)
+        if not m:
+            raise RuntimeError(f'Unparsable WKT: {value}')
+        if m[1] == 'POINT':
+            self.value = self._parse_wkt_coord(m[2])
+        elif m[1] == 'LINESTRING':
+            self.value = self._parse_wkt_line(m[2])
+        elif m[1] == 'POLYGON':
+            self.value = [self._parse_wkt_line(ln) for ln in m[2][1:-1].split('),(')]
+
+    def _parse_wkt_coord(self, coord):
+        return tuple(DBValueFloat(float(f.strip()), self.precision) for f in coord.split())
+
+    def _parse_wkt_line(self, coords):
+        return [self._parse_wkt_coord(pt) for pt in coords.split(',')]
+
+    def __eq__(self, other):
+        if other.find(',') < 0:
+            geom = self._parse_input_coord(other)
+        elif other.find('(') < 0:
+            geom = self._parse_input_line(other)
+        else:
+            geom = [self._parse_input_line(ln) for ln in geom.strip()[1:-1].split('),(')]
+
+        return self.value == geom
+
+    def _parse_input_coord(self, other):
+        coords = other.split(' ')
+        if len(coords) == 1:
+            return self.factory.grid_node(int(coords[0]))
+        if len(coords) == 2:
+            return tuple(float(c.strip()) for c in coords)
+
+        raise RuntimeError(f'Bad coordinate: {other}')
+
+    def _parse_input_line(self, other):
+        return [self._parse_input_coord(pt.strip()) for pt in other.split(',')]
+
+    def __repr__(self):
+        return self.orig_value
 
 
 class DBValueFloat:
