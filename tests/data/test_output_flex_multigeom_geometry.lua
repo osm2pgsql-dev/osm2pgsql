6c26079b830d716b0e0c65c948720a3e98b83960
Sarah Hoffmann <lonvia@denofr.de>
2022-05-29 19:25:48 +0200

    convert tests/test-output-flex-multigeom to BDD test

--- tests/data/test_output_flex_multigeom_geometry.lua
+++
@@ -1,5 +0,0 @@
-
-test = { type = 'geometry' }
-
-dofile(osm2pgsql.config_dir .. '/test_output_flex_multigeom.lua')
-
--- tests/data/test_output_flex_multigeom_geometry_split.lua
+++
@@ -1,5 +0,0 @@
-
-test = { type = 'geometry', split_at = 'multi' }
-
-dofile(osm2pgsql.config_dir .. '/test_output_flex_multigeom.lua')
-
