a26529cae97244c02678c2b66db1bec23c260093
Jochen Topf <jochen@topf.org>
2022-11-12 17:56:17 +0100

    Use const/noexcept where possible

--- src/pgsql.hpp
+++ src/pgsql.hpp
@@ -37,7 +37,7 @@
 class pg_result_t
 {
 public:
-    pg_result_t() = default;
+    pg_result_t() noexcept = default;
 
     explicit pg_result_t(PGresult *result) noexcept : m_result(result) {}
 
@@ -112,7 +112,7 @@ public:
     }
 
     /// Return true if this holds an actual result.
-    explicit operator bool() { return m_result.get(); }
+    explicit operator bool() const noexcept { return m_result.get(); }
 
 private:
     struct pg_result_deleter_t
