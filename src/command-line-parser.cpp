osm2pgsql-dev/osm2pgsql

commit 2449b294735c7cc59e4ee9acc4ef0473391175d4

Do not print timestamp before the version when --version is used

src/command-line-parser.cpp
@@ -238,6 +238,7 @@ void check_options_expire(options_t *options) {
 
 void print_version()
 {
+    fmt::print(stderr, "osm2pgsql version {}", get_osm2pgsql_version());
     fmt::print(stderr, "Build: {}\n", get_build_type());
     fmt::print(stderr, "Compiled using the following library versions:\n");
     fmt::print(stderr, "Libosmium {}\n", LIBOSMIUM_VERSION_STRING);
@@ -609,15 +610,15 @@ options_t parse_command_line(int argc, char *argv[])
         return options;
     }
 
+    if (app.want_version()) {
+        options.command = command_t::version;
+        return options;
+    }
+
     log_info("osm2pgsql version {}", get_osm2pgsql_version());
 
     if (verbose) {
         get_logger().set_level(log_level::debug);
-    }
-
-    if (app.want_version()) {
-        options.command = command_t::version;
-        return options;
     }
 
     if (options.append && app.count("--create")) {

commit 62f77d3f469807ac81f3c78a402160f44b1bdd31

Add missing return in version output
This was broken in 2449b294735c7cc59e4ee9acc4ef0473391175d4

src/command-line-parser.cpp
@@ -259,7 +259,7 @@ void check_options_expire(options_t *options) {
 
 void print_version()
 {
-    fmt::print(stderr, "osm2pgsql version {}", get_osm2pgsql_version());
+    fmt::print(stderr, "osm2pgsql version {}\n", get_osm2pgsql_version());
     fmt::print(stderr, "Build: {}\n", get_build_type());
     fmt::print(stderr, "Compiled using the following library versions:\n");
     fmt::print(stderr, "Libosmium {}\n", LIBOSMIUM_VERSION_STRING);